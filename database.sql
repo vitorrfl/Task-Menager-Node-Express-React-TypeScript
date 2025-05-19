5df7f47d2d61ab509c24f1bd67547f1a8f79d085
vitorrfl <vitornewof@icloud.com>
2025-05-19 18:17:25 -0300

    fix

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE task_manager;
+\c task_manager
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(50) DEFAULT 'pendente'
+);
